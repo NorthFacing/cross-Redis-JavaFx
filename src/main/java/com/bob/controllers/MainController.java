1d0c5f04af6ab9251f297d3aeace1ffbceaedce0
Bob <0haizhu0@gmail.com>
2017-12-29 09:08:33 +0800

    页面布局调整

--- src/main/java/com/bob/controllers/MainController.java
+++ src/main/java/com/bob/controllers/MainController.java
@@ -1,8 +1,15 @@
 package com.bob.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.SplitPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,10 +17,37 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
 
 	@FXML
+	private StackPane root;
+	@FXML
 	private SplitPane centrePane;
 
 	@FXML
 	private void addConn() {
+
+//		JFXTextField username = new JFXTextField();
+//		username.setPromptText("username");
+//		JFXPasswordField password = new JFXPasswordField();
+//		password.setPromptText("password");
+//
+//		StackPane content = new StackPane();
+//		content.getChildren().addAll(username,password);
+//
+//		JFXDialogLayout layout = new JFXDialogLayout();
+//		layout.setHeading(new Text("新增连接"));
+//		layout.setBody(content);
+//
+//		JFXButton okay = new JFXButton("确定");
+//		JFXButton cancle = new JFXButton("取消");
+//		layout.setActions(cancle, okay);
+//
+//		JFXDialog dialog = new JFXDialog(root, layout, JFXDialog.DialogTransition.CENTER, false);
+//
+//		okay.setOnAction(e -> {
+//			System.out.println("Okay");
+//		});
+//		cancle.setOnAction(e -> dialog.close());
+//
+//		dialog.show();
 
 	}
 
