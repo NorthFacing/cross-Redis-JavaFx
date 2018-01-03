908e1eced60239f355f24c39f054d19fff6708e1
Bob <0haizhu0@gmail.com>
2018-01-03 23:41:36 +0800

    [N] :smiling_imp: add lettuce & lomlok libraries with bug to fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,17 @@
 module bob.crossRedis {
 
 	requires java.base;
-	requires java.logging; // TODO 是否使用此日志包？
+	requires java.logging;
 	requires java.desktop;
 	requires javafx.fxml;
 	requires javafx.controls;
 
-//	requires slf4j.api;
-
 	requires com.jfoenix;
 	requires de.jensd.fx.glyphs.fontawesome;
+
+	requires lettuce;
+
+	requires lombok;
 
 	exports bob.crossRedis;
 	exports bob.crossRedis.controllers;
